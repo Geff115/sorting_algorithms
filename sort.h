4311791a6424853c26fdfad9533f411fc3fb6cd6
gabrielnoah129@gmail.com <Geff115>
2024-04-26 04:29:20 +0100

    Header file coming in hot

---
+++ sort.h
@@ -0,0 +1,20 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+#endif
