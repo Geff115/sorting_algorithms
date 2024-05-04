3a99084801b03de15106e30453bdf63f008994fa
gabrielnoah129@gmail.com <Geff115>
2024-05-04 12:43:53 +0100

    Updated our header file

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
-int lomuto_partition(int *array, int start, int end);
+int lomuto_partition(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif
