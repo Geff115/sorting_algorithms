9c9a98527d7292dbc45dbf2794ebc39fece556ac
Geff115 <gabrielnoah129@gmail.com>
2024-08-06 23:52:19 +0100

    A function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,30 +6,19 @@
  * @node1: first node in the list.
  * @node2: second node in the list.
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2)
 {
-	listint_t *tmp_prev, *tmp_next;
-
-	if (list == NULL || *list == NULL || node1 == NULL || node2 == NULL)
-	{
-		return;
-	}
-	if (node1->next != NULL)
-		node1->next->prev = node2;
-	if (node2->prev != NULL)
-		node2->prev->next = node1;
-	tmp_next = node1->next;
-	tmp_prev = node1->prev;
-	node1->next = node2->next;
-	node1->prev = node2;
-	node2->next = tmp_next;
-	node2->prev = tmp_prev;
-	if (tmp_next != NULL)
-		tmp_next->prev = node1;
-	if (tmp_prev != NULL)
-		tmp_prev->next = node2;
-	if (node1 == *list)
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
 		*list = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
@@ -39,21 +28,19 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node1, *node2;
+	listint_t *node1, *node2, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	node2 = (*list)->next;
-	while (node2 != NULL)
+	
+	for (node1 = (*list)->next; node1 != NULL; node1 = tmp)
 	{
-		node1 = node2->prev;
-		while (node1 != NULL && node2->n < node1->n)
+		tmp = node1->next;
+		node2 = node1->prev;
+		while (node2 != NULL && node1->n < node2->n)
 		{
-			swap_nodes(list, node2, node1);
-			print_list(*list);
-			node2 = node1;
-			node1 = node2->prev;
+			swap_nodes(list, &node2, node1);
+			print_list((const listint_t *)*list);
 		}
-		node2 = node2->next;
 	}
 }
