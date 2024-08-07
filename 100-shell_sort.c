549c38be6d7ac061ab46935c732b31a9a6e28e17
Geff115 <gabrielnoah129@gmail.com>
2024-08-07 11:25:20 +0100

    A  function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,70 +1,17 @@
 #include "sort.h"
 
 /**
- * Knuth_sequence_calculation - This function generates the Knuth
- * sequence interval based on the size of the array.
- * @size: The size of the array.
- * @num_elements: Number of elements in the array.
- *
- * Return: An array containing the intervals to be used in the shell
- * sort algorithm.
+ * swap - This function swaps the nodes of two elements.
+ * @a: Element to be swapped.
+ * @b: Element to be swapped.
  */
-int *Knuth_sequence_calculation(size_t size, int *num_elements)
+void swap(int *a, int *b)
 {
-	int *sequence;
-	size_t i, interval = 1;
+	int temp;
 
-	*num_elements = 0;
-	while (interval < size)
-	{
-		interval = interval * 3 + 1;
-		(*num_elements)++;
-	}
-	sequence = malloc(sizeof(int) * (*num_elements));
-	if (sequence == NULL)
-	{
-		fprintf(stderr, "Error allocating memory for the sequence\n");
-		return (NULL);
-	}
-	i = *num_elements - 1;
-	interval = 1;
-	while (interval < size)
-	{
-		sequence[i] = interval;
-		interval = interval * 3 + 1;
-		if (interval >= size)
-			break;
-		i--;
-	}
-	sequence[i] = 1;
-	return (sequence);
-}
-
-/**
- * shell_insertion_sort - This function implements the insertion sort algorithm
- * in an unsorted array using a gap index.
- * @array: Array of elements.
- * @size: Size of the array.
- * @gap: gap element in a certain index from the next element's index.
- *
- */
-void shell_insertion_sort(int *array, size_t size, int gap)
-{
-	int curr_elem, curr_idx;
-	size_t i = 0;
-
-	while (i < size)
-	{
-		curr_idx = i;
-		curr_elem = array[i];
-		while ((curr_idx >= gap) && (array[curr_idx - gap] > curr_elem))
-		{
-			array[curr_idx] = array[curr_idx - gap];
-			curr_idx -= gap;
-		}
-		array[curr_idx] = curr_elem;
-		i++;
-	}
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -76,13 +23,27 @@ void shell_insertion_sort(int *array, size_t size, int gap)
  */
 void shell_sort(int *array, size_t size)
 {
-	int *Knuth_sequence, i, num_elements;
+	size_t gap, i, j;
 
-	Knuth_sequence = Knuth_sequence_calculation(size, &num_elements);
-	for (i = num_elements - 1; i >= 0; i--)
+	if (array == NULL || size < 2)
+		return;
+
+	/*Calculate the initial gap using the Knuth sequence*/
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	/*Reduce the gap size using the Knuth sequence*/
+	for (; gap >= 1; gap /= 3)
 	{
-		shell_insertion_sort(array, size, Knuth_sequence[i]);
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
 		print_array(array, size);
 	}
-	free(Knuth_sequence);
 }
